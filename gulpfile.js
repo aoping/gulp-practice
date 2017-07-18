b11fd97dc9d5a717a4e3b7252f1b02e07a9ca696
zoro <aoping@yougola.com>
2017-07-18 10:06:47 +0800

    less

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,17 @@ var filter = require('gulp-filter');
 var uglify = require('gulp-uglify');
 var browserify = require('gulp-browserify'); // 用于把依赖也打进同一个包
 var reload = browserSync.reload;
+var less = require('gulp-less');
+var cssmin = require('gulp-minify-css');
+//确保本地已安装gulp-sourcemaps [cnpm install gulp-sourcemaps --save-dev]
+var sourcemaps = require('gulp-sourcemaps'); // 审查元素时会是在源文件里
+//当发生异常时提示错误 确保本地安装gulp-notify和gulp-plumber
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+
 
 // 静态服务器 + 监听 scss/html 文件
-gulp.task('serve', ['sass', 'js', 'html'], function() {
+gulp.task('serve', ['sass', 'less', 'js', 'html'], function() {
 
     browserSync.init({
         server: "./dist"
@@ -16,6 +24,7 @@ gulp.task('serve', ['sass', 'js', 'html'], function() {
     // 所有的浏览器重载后任务完成。
     gulp.watch("app/js/*.js", ['js-watch']);
     gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/less/*.less", ['less']);
     gulp.watch("app/*.html", ['html']).on('change', reload);
 });
 
@@ -44,5 +53,17 @@ gulp.task('html', function() {
 // 创建一个任务确保JS任务完成之前能够继续响应
 // 浏览器重载
 gulp.task('js-watch', ['js'], browserSync.reload);
+
+
+//定义一个testLess任务（自定义任务名称）
+gulp.task('less', function() {
+    gulp.src('app/less/index2.less') //该任务针对的文件
+        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+        .pipe(sourcemaps.init())
+        .pipe(less()) //该任务调用的模块
+        .pipe(cssmin()) // 兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css')); //将会在src/css下生成index.css
+});
 
 gulp.task('default', ['serve']);
