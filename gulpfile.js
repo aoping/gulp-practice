c4b2a99ddbffb027dcae0e5d066d7dee8f88e9f8
zoro <aoping@yougola.com>
2017-07-18 10:42:46 +0800

    cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,8 @@ var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant'); // 使用pngquant深度压缩png图片的imagemin插件
 var cache = require('gulp-cache');
+var cssmin = require('gulp-minify-css');
+var cssver = require('gulp-make-css-url-version');
 
 
 // 静态服务器 + 监听 scss/html 文件
@@ -145,5 +147,18 @@ gulp.task('testImagemin', function() {
             use: [pngquant()]
         })))
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('testCssmin', function() {
+    gulp.src('src/css/*.css')
+        .pipe(cssver()) //给css文件里引用文件加版本号（文件MD5）
+        .pipe(cssmin({
+            advanced: false, //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7', //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true, //类型：Boolean 默认：false [是否保留换行]
+            keepSpecialComments: '*'
+                //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+        }))
+        .pipe(gulp.dest('dist/css'));
 });
 gulp.task('default', ['serve']);
