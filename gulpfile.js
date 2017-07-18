fcc54478ba583ef8f082c6af38685b561334ee11
zoro <aoping@yougola.com>
2017-07-18 10:49:33 +0800

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -167,4 +167,16 @@ gulp.task('testRev', function() {
         .pipe(rev())
         .pipe(gulp.dest('dist/html'));
 });
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify');
+
+gulp.task('jsmin', function() {
+    gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
+        .pipe(uglify({
+            mangle: true, //类型：Boolean 默认：true 是否修改变量名
+            compress: true, //类型：Boolean 默认：true 是否完全压缩
+            preserveComments: 'all' //保留所有注释
+        }))
+        .pipe(gulp.dest('dist/js'));
+});
 gulp.task('default', ['serve']);
